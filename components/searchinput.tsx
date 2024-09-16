117316d79c352aff2ed7ac4212c64e5b27480c00
Shadow <dark35854@gmail.com>
2024-09-16 10:23:57 +0530

    prisma_init

--- components/searchinput.tsx
+++ components/searchinput.tsx
@@ -1,12 +1,39 @@
 "use client"
-
+import qs from "query-string"
+import { ChangeEventHandler, useEffect, useState } from "react"
 import { Search } from "lucide-react"
 import { Input } from "./ui/input"
+import { useRouter } from "next/navigation"
+import { useSearchParams } from "next/navigation"
+import { useDebounce } from "@/hooks/use-debounce"
 
 export const SearchInput=()=>{
+    const navigate = useRouter();
+    const searchParams =useSearchParams()
+    const categoryId = searchParams.get("categoryId")
+    const name = searchParams.get("name")
+    const [value, setValue] = useState(name || "")
+    const debouncedValue = useDebounce<string>(value, 500)
+    const onchange:ChangeEventHandler<HTMLInputElement>=(e)=>{
+        setValue(e.target.value)
+    }
+    useEffect(()=>{
+        const query = {
+            name: debouncedValue,
+            categoryId:categoryId
+
+        }
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query
+        } ,{skipEmptyString:true ,skipNull:true})
+        navigate.push(url)
+    },[debouncedValue,navigate,categoryId]);
     return <div className="relative">
         <Search className=" absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-        <Input
+        <Input 
+         onChange={onchange}
+         value={value}
          placeholder="Search..."
          className="pl-10 bg-primary/10"
         />
